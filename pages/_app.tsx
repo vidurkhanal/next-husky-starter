3a28f3e092ce67c4849f14658b992c73ba7b2248
vidurkhanal <vkhanal@caldwell.edu>
2022-11-19 13:29:12 -0500

    feat: all deps installed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ReactElement } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+    Component,
+    pageProps,
+}: AppProps): ReactElement<any, any> {
+    return <Component {...pageProps} />;
 }
